bac81f6b5d84334be1e435da38ede8d711872035
AydanMacGregor <macgregoraydan@gmail.com>
2024-02-08 10:05:59 -0500

    Fixed Slashing and Animation

--- Assets/Characters/Tod/Scripts/AttackDefend.cs
+++ Assets/Characters/Tod/Scripts/AttackDefend.cs
@@ -6,10 +6,11 @@ public class AttackDefend : MonoBehaviour
 {
     public GameObject slash;
     public GameObject tod;
+    Vector2 dir;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dir = tod.GetComponent<movement>().direction;
     }
 
     // Update is called once per frame
@@ -17,7 +18,7 @@ public class AttackDefend : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(slash, new Vector3(tod.transform.position.x + (0.5f * Input.GetAxis("Horizontal")), tod.transform.position.y, tod.transform.position.z), Quaternion.identity);
+            Instantiate(slash, new Vector3(tod.transform.position.x + (0.5f * dir.x), tod.transform.position.y + (0.5f * dir.y), tod.transform.position.z), Quaternion.identity);
         }
     }
 }
