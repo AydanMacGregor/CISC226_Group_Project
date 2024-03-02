0422446e2eb3847d1e6e8ae612d9b4d342841bed
AydanMacGregor <macgregoraydan@gmail.com>
2024-03-01 21:16:22 -0500

    Fixed Healthbar Issue

--- Assets/Characters/Tod/Scripts/HealthSystem.cs
+++ Assets/Characters/Tod/Scripts/HealthSystem.cs
@@ -47,16 +47,15 @@ public class HealthSystem : MonoBehaviour
     {
         if (!gameObject.GetComponent<AttackDefend>().s)
         {
-            if (other.gameObject.name == "Soul")
+            
+            if (other.gameObject.GetComponent<SpriteRenderer>().sprite == Charge)
             {
-                if (other.gameObject.GetComponent<SpriteRenderer>().sprite == Charge)
-                {
-                    damage(chargeDamageAmount);
-                }
-                else if (other.gameObject.GetComponent<SpriteRenderer>().sprite == eyeball)
-                {
-                    damage(eyeballDamageAmount);
-                }
+                damage(chargeDamageAmount);
+            }
+            else if (other.gameObject.GetComponent<SpriteRenderer>().sprite == eyeball)
+            {
+                Debug.Log("Hit");
+                damage(eyeballDamageAmount);
             }
             if (other.gameObject.name == "Bat" || other.gameObject.name == "Bat(Clone)")
             {
