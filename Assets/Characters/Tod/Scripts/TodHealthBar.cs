feb7c3fb5dac5cafabe91aa46e37ea5b14181b75
Samak15 <makyisin666@gmail.com>
2024-03-02 12:51:39 -0500

    Adding Name Tag and Changes

--- Assets/Characters/Tod/Scripts/TodHealthBar.cs
+++ Assets/Characters/Tod/Scripts/TodHealthBar.cs
@@ -7,21 +7,16 @@ public class TodHealthBar : MonoBehaviour
 {
 
     public Slider slider;
-    public Gradient gradient;
     public Image fill;
 
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
-
-        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
-
-        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
--- Assets/Scenes/DenialBoss/Boss/Scripts/bossHealthBar.cs
+++ Assets/Scenes/DenialBoss/Boss/Scripts/bossHealthBar.cs
@@ -7,21 +7,16 @@ public class bossHealthBar : MonoBehaviour
 {
 
     public Slider slider;
-    public Gradient gradient;
     public Image fill;
 
     public void SetMaxHealth(float health)
     {
         slider.maxValue = health;
         slider.value = health;
-
-        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(float health)
     {
         slider.value = health;
-
-        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
