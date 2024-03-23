90572e1a039bcd49feb51fc4815b0342ff665aa5
AydanMacGregor <macgregoraydan@gmail.com>
2024-03-22 20:00:32 -0400

    Pause Menu

---
+++ Assets/Characters/PauseandPlay/PauseGame.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    [SerializeField] GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ContinueOrPauseGame();
+        }
+    }
+
+    public void ContinueOrPauseGame() 
+    {
+        isPaused = isPaused ? false : true;
+        Time.timeScale = isPaused ? 0 : 1;
+        pauseMenu.SetActive(isPaused);
+    }
+
+    public void Exit()
+    {
+        ContinueOrPauseGame();
+        string name = "MainMenuScene";
+        SceneManager.LoadScene(name);
+    }
+}
